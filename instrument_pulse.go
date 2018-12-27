1e6702c27696071529867f911ff29ae74ca08e85
Savino Pio Liguori <savino.liguori@gmail.com>
2018-12-27 11:09:09 +0100

    Pulse instrument - Write - Little modification

--- instrument_pulse.go
+++ instrument_pulse.go
@@ -105,9 +105,7 @@ func (i *instrumentPulse) read(in *instrument, r io.ReadSeeker) {
 func (i *instrumentPulse) write(in *instrument, w io.WriteSeeker, version byte) {
 	var b byte
 
-	b = 0
-
-	_ = writeByte(b, w)
+	_ = writeByte(0, w)
 	_ = writeByte(in.envelopeVolume, w)
 	_ = writeByte(i.pulse2tune, w)
 
